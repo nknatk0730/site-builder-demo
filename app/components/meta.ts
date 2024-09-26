1ae5a36e8f8b26183397535616c4722a9271f55f
nknatk0730 <nknatk0730@gmail.com>
2024-09-26 15:24:45 +0900

    fix:(meta.ts) add id props

--- app/components/meta.ts
+++ app/components/meta.ts
@@ -3,7 +3,11 @@ import { Hero } from "./hero";
 import { Features } from "./features";
 
 export const Components: {
-  [key in SectionType]: () => JSX.Element;
+  [key in SectionType]: ({
+    id,
+  }: {
+    id: string;
+  }) => JSX.Element;
 } = {
   hero: Hero,
   features: Features,
